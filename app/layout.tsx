b57302221720ec850ecf4b2dafceda1c109aac4d
alann13 <alan.nguy@outlook.com>
2025-05-03 16:33:58 -0400

    changed fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Outfit } from 'next/font/google'
 import './globals.css'
 import Navigation from './components/Navigation'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const outfitSans = Outfit({
+  variable: '--font-outfit-sans',
   subsets: ['latin'],
 })
 
 export const metadata: Metadata = {
-  title: 'Some entertainment app',
+  title: 'Chillflixed',
   description: 'A description for some entertainment app',
 }
 
@@ -26,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased p-[2rem] grid grid-cols-[6rem_1fr] gap-x-[2.25rem]`}
+        className={`${outfitSans.className} antialiased p-[2rem] grid grid-cols-[6rem_1fr] gap-x-[2.25rem]`}
       >
         <header>
           <Navigation />
